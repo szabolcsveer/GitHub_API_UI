40198604bbe67eb65b66caebb6f940dfec84ee76
Szabolcs Veer <szabolcs.veer@gmail.com>
2019-10-06 10:41:49 +0200

    feat(add.module.ts) installed and imported dependencies, created httpLink after injecting Apollo service

--- GithubApi/src/app/app.module.ts
+++ GithubApi/src/app/app.module.ts
@@ -6,9 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, Apollo } from "apollo-angular";
+import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+
 //Angular Material
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 //Components
 import { InputfieldComponent } from './components/inputfield/inputfield.component';
@@ -27,10 +32,25 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     MatInputModule,
     MatButtonModule,
     RouterModule,
-    HttpClientModule
+    HttpClientModule,
+    ApolloModule,
+    Apollo,
+    HttpLinkModule,
+    HttpLink,
+    InMemoryCache
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink
+  ) {
+    apollo.create({
+      link: httpLink.create({uri:'https://api.github.com/graphql'}),
+      cache: new InMemoryCache()
+    })
+  }
+}
 
